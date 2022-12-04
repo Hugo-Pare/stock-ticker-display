bbe7cc67f974660f193d9a68b5d28c200fdce333
Hugo Paré <71098494+Hugo-Pare@users.noreply.github.com>
2022-12-03 21:53:46 -0500

    Testing

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class RunText(SampleBase):
         textLine1 = "Invesco QQQ Trust (QQQ)"
         textLine2 = "211.89 (-18.44%)"
 
-        line1 = graphics.DrawText(offscreen_canvas, font, pos, 12, textColor, textLine1)
+        line1 = graphics.DrawText(offscreen_canvas, font, pos, 12, textColorUp, textLine1)
         line2 = graphics.DrawText(offscreen_canvas, font, pos, 28, textColorDown, textLine2)
 
         while True:
